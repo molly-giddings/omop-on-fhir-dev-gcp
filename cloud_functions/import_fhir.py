9daf57593fd1732d55274056d49a8dacaba82359
molly-giddings <89104288+molly-giddings@users.noreply.github.com>
2021-10-21 17:44:16 -0500

    Create import_fhir.py

---
+++ cloud_functions/import_fhir.py
@@ -0,0 +1,61 @@
+
+def import_fhir(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+
+    import json
+    import os
+    import requests
+    from google.cloud import tasks_v2
+
+    file = event
+    print(f"Processing file: {file['name']}.")
+
+    # Set the queue and cloud function variables.
+    project = os.environ.get('GCP_PROJECT','MY_PROJECT')    
+    queue_name = os.environ.get('TASK_QUEUE', 'import-fhir-queue')
+    location = os.environ.get('TASK_QUEUE_LOCATION', 'MY_LOCATION')
+    import_function = os.environ.get('FHIR_IMPORT_FUNCTION', 'fhir_to_fhirstore')
+
+    url = f"https://{location}-{project}.cloudfunctions.net/{import_function}"
+
+    # Create a client.
+    client = tasks_v2.CloudTasksClient()
+
+    # Construct the fully qualified queue name.
+    parent = client.queue_path(project, location, queue_name)
+
+    # Get the queue object.
+    queue = client.get_queue(request=None, name=parent)
+
+    # Create the task queue if it doesn't exist; update if it already exists.
+    client.update_queue(request=None, queue=queue)
+
+    service_account_email = "MY_SERVICE_ACCOUNT_EMAIL"
+    audience = url
+
+    # Construct the request body.
+    task = {
+        "http_request": {  # Specify the type of request.
+            "http_method": tasks_v2.HttpMethod.POST,
+            "url": url,  # The full url path that the task will be sent to.
+            "oidc_token": {"service_account_email": service_account_email, "audience": audience},
+        }
+    }
+
+    if isinstance(file, dict):
+        # convert dict to JSON string
+        file = json.dumps(file)
+
+    if file is not None:
+        # The API expects a payload of type bytes
+        converted_payload = file.encode()
+
+        # Add the payload to the request body
+        task['http_request']['body'] = converted_payload  
+    
+    # Use the client to build and send the task.
+    response = client.create_task(request={"parent": parent, "task": task})
